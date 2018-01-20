ef314d88f6ab36147b6fafd9db5c4bdb5086a58a
leb yep <max.leboiteux@gmail.com>
2018-01-20 10:45:27 +0100

    Création de la classe Main provisoire pour tester la cohérence des développements précédents

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,7 +1,16 @@
 package main.java;
 
-public class Main {
+import static main.java.mecaniqueDeJeux.Game.*;
 
+public class Main {
+    public static void main(String[] args)
+
+    {
+        System.out.println("----------------Enregistrer le code----------------");
+        registercombinaisonsecrete();
+        System.out.println("----------------Trouver le code----------------");
+        findcombinaisonsecrete();
+    }
 
 
 
