34f199529163b86c3095173a7bd2f81b0312fa77
leb yep <max.leboiteux@gmail.com>
2018-01-22 19:49:43 +0100

    Création du tirage au sort de la combinaison par lIA

--- src/main/java/mecaniqueDeJeux/IA.java
+++ src/main/java/mecaniqueDeJeux/IA.java
@@ -1,4 +1,26 @@
 package main.java.mecaniqueDeJeux;
 
+import java.util.Random;
+
+/**
+ *
+ * Intellignece artificielle recherchant la combinaison secère ou créant la combinaison secrète
+ *
+ */
+
+
 public class IA {
+
+public static String IACombiSecrète (int nbchar){
+    String CombiIA="";
+    Random randomGenerator = new Random();
+    int randomInt ;
+    System.out.println(CombiIA);
+    for (int i=0; i<nbchar;i++){
+        randomInt = randomGenerator.nextInt(10);
+        CombiIA=CombiIA+Integer.toString(randomInt);
+    }
+    return CombiIA;
+}
+
 }
