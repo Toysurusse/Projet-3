d4b97e9e665c7cf389365254e367a018df6aba15
leb yep <max.leboiteux@gmail.com>
2018-01-22 14:45:48 +0100

    Mise à jour de la classe Main pour instancier RecherchePlusMoins

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,15 +1,13 @@
 package main.java;
 
-import static main.java.mecaniqueDeJeux.Game.*;
+
+import main.java.mecaniqueDeJeux.RecherchePlusMoins;
 
 public class Main {
     public static void main(String[] args)
 
     {
-        System.out.println("----------------Enregistrer le code----------------");
-        registercombinaisonsecrete();
-        System.out.println("----------------Trouver le code----------------");
-        findcombinaisonsecrete();
+        RecherchePlusMoins partie = new RecherchePlusMoins();
     }
 
 
