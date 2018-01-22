bbda308894abf6670b89fc6cfd106d5418f7c911
leb yep <max.leboiteux@gmail.com>
2018-01-22 19:45:50 +0100

    Initialisation par défaut en mode développeur

--- src/main/java/mecaniqueDeJeux/Configuration.java
+++ src/main/java/mecaniqueDeJeux/Configuration.java
@@ -3,12 +3,12 @@ package main.java.mecaniqueDeJeux;
 public class Configuration {
 
     public static int nbcasesRech = 4;
-    public static int nbessaiRech = 10;
+    public static int nbessaiRech = 2;
 
     public static int nbcasesMasterM = 4;
     public static int nbessaiMasterM = 10;
     public static int nbcouleurMasterM = 5;
 
-    public static boolean ModeDev = false;
+    public static boolean ModeDev = true;
 
 }
