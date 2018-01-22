9d6fee7947ebd3ad3a76655769cec9a83ad16499
leb yep <max.leboiteux@gmail.com>
2018-01-22 14:44:44 +0100

    Utilisation de la classe plus moins en tant qu'extension de Game

--- src/main/java/mecaniqueDeJeux/RecherchePlusMoins.java
+++ src/main/java/mecaniqueDeJeux/RecherchePlusMoins.java
@@ -1,4 +1,15 @@
 package main.java.mecaniqueDeJeux;
 
-public class RecherchePlusMoins {
+public class RecherchePlusMoins extends Game {
+
+    public RecherchePlusMoins (){
+
+        System.out.println("----------------Enregistrer le code----------------");
+        registercombinaisonsecrete(Configuration.nbcasesRech," chiffres de 0 à 9");
+        System.out.println("------------------Trouver le code------------------");
+        findcombinaisonsecrete(Configuration.nbcasesRech, Configuration.nbessaiRech," chiffres de 0 à 9");
+
+    }
+
+
 }
